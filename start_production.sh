385e99e39f39af815328ac91cf35fd3ec05e565d
Alexander Veit <53857412+alexander-veit@users.noreply.github.com>
2020-08-14 18:01:41 -0400

    Higlass docker setup

---
+++ start_production.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+set -e
+set -v
+
+
+# DOCKER_VERSION is the version of higlass/higlass-docker
+# docker pull higlass/higlass-docker:v0.6.51
+DOCKER_VERSION=v0.6.51
+IMAGE=higlass/higlass-docker:$DOCKER_VERSION
+PORT=80
+FILE_VERSION=20200814
+
+# stop all containers
+docker ps -a -q | xargs sudo docker stop
+# remove all containers
+docker ps -a -q | xargs sudo docker rm
+
+
+docker run --name higlass-container \
+           --publish $PORT:80 \
+           --volume ~/hg-data:/data \
+           --volume ~/hg-tmp:/tmp \
+           --detach \
+           $IMAGE
+
+docker exec higlass-container python higlass-server/manage.py ingest_tileset \
+            --filename /data/hg38_full.txt \
+            --filetype chromsizes-tsv \
+            --datatype chromsizes \
+            --coordSystem hg38
+
+docker exec higlass-container python higlass-server/manage.py ingest_tileset \
+            --filename /data/transcripts_$FILE_VERSION.beddb \
+            --filetype beddb --coordSystem hg38 \
+            --datatype gene-annotation \
+            --uid transcripts_$FILE_VERSION
+
+docker exec higlass-container python higlass-server/manage.py ingest_tileset \
+            --filename /data/canonical_transcripts_$FILE_VERSION.beddb \
+            --filetype beddb --coordSystem hg38 \
+            --datatype gene-annotation \
+            --uid canonical_transcripts_$FILE_VERSION
