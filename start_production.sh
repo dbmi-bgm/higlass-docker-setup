bd03570f99544b124910fb9a1ac8ddadfc9b3277
Alexander Veit <53857412+alexander-veit@users.noreply.github.com>
2020-08-14 19:14:57 -0400

    Introduce idle time to make sure the container is properly running

--- start_production.sh
+++ start_production.sh
@@ -2,7 +2,6 @@
 set -e
 set -v
 
-
 # DOCKER_VERSION is the version of higlass/higlass-docker
 # docker pull higlass/higlass-docker:v0.6.51
 DOCKER_VERSION=v0.6.51
@@ -15,8 +14,7 @@ docker stop higlass-container || true && docker rm higlass-container || true
 
 # remove previously ingested data
 rm -f ~/hg-data/db.sqlite3
-rm -rf ~/hg-data/media
-
+rm -rf ~/hg-data/media ~/hg-data/media
 
 docker run --name higlass-container \
            --publish $PORT:80 \
@@ -24,6 +22,9 @@ docker run --name higlass-container \
            --volume ~/hg-tmp:/tmp \
            --detach \
            $IMAGE
+
+# We have to wait to make sure the container is properly running
+sleep 2
 
 docker exec higlass-container python higlass-server/manage.py ingest_tileset \
             --filename /data/hg38_full.txt \
