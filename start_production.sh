df81f45dcd59b53866cdc060a3ee3a0f61d12528
Alexander Veit <53857412+alexander-veit@users.noreply.github.com>
2020-09-02 13:24:50 -0400

    Add ClinVar data

--- start_production.sh
+++ start_production.sh
@@ -46,3 +46,10 @@ docker exec higlass-container python higlass-server/manage.py ingest_tileset \
             --coordSystem hg38 \
             --datatype gene-annotation \
             --uid canonical_transcripts_hg38
+
+# ClinVar data version 20200824
+docker exec higlass-container python higlass-server/manage.py ingest_tileset \
+            --filename /data/clinvar_$FILE_VERSION.beddb \
+            --filetype beddb \
+            --datatype bedlike \
+            --uid clinvar_20200824_hg38
